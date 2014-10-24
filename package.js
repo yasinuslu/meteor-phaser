1d52ac691aa6b1be1ac54d290c78cc413cd92421
Yasin Uslu <nepjua@gmail.com>
2014-10-25 02:59:18 +0300

    remove unnecesary, add comment about api.export

--- package.js
+++ package.js
@@ -2,12 +2,17 @@ Package.describe({
   name: 'yasinuslu:phaser',
   summary: 'Phaser-IO repackaged for meteor',
   version: '1.0.0',
-  git: ''
+  git: 'https://github.com/yasinuslu/meteor-phaser.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
   api.addFiles('lib/phaser.js', 'client');
+
+  // since Phaser and PIXI vars aren't package spec variables we can't export it
+  // but phaser sets window.Phaser and window.PIXI anyway
+  // it might cause security concerns but MDG must have thought this deeply so we're leaving it there
+  // api.export('Phaser', 'client');
 });
 
 Package.onTest(function(api) {
